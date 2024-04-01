db2e9ba0b97ed91558c8f1211f6c01bb2931d45e
adriatls <adria.tavares28@gmail.com>
2024-04-01 08:19:39 -0300

    feat: creating basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserList from "./pages/UserList";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
 
 function App() {
-
-  return <div>React App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
